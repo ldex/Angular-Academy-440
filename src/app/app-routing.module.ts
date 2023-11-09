0139f75ecaa3af01ab738d23af1c111024256024
ldex <laurent.duveau+git@gmail.com>
2023-11-09 11:49:20 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/home.component';
+import { ContactComponent } from './shared/contact.component';
+import { AdminComponent } from './shared/admin.component';
+import { ErrorComponent } from './shared/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch:'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
