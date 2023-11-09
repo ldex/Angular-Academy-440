edc6ebcb26ab58e34ba8d76144a2b40e1a0208d4
ldex <laurent.duveau+git@gmail.com>
2023-11-09 09:50:25 -0500

    Error handling

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.interface';
-import { Observable, delay } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, delay, tap, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -20,8 +20,28 @@ export class ProductService {
                       .http
                       .get<Product[]>(this.baseUrl)
                       .pipe(
-                        delay(1500) // For the demo!!
+                        tap(console.table),
+                        delay(1500), // For the demo!!
+                        catchError(this.handleError)
                       )
+  }
+
+
+  private handleError(error: HttpErrorResponse) {
+    // in a real world app, you may send the error to the server using some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMsg: string;
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMsg = 'An error occurred:' + error.error.message;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      errorMsg = `Backend returned code ${error.status}, body was: ${error.error}`;
+    }
+    console.error(errorMsg);
+    // return an observable with a user-facing error message
+    return throwError(() => 'Something bad happened; please try again later.');
   }
 
 }
